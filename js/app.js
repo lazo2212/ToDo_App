a737711dd28369a6b3dae2634da74a1d321fbc41
Robert Lazić <robert.lazicpz@gmail.com>
2023-02-14 21:06:38 +0100

    modified:   css/style.css 	modified:   index.html 	modified:   js/app.js 	modified:   sass/_buttons.scss 	modified:   sass/_config.scss 	modified:   sass/_utilities.scss 	modified:   sass/style.scss

---
+++ js/app.js
@@ -0,0 +1,75 @@
+// SELECTORS
+const inputField = document.querySelector('input');
+const submitBtn = document.querySelector('#submitBtn');
+const container = document.querySelector('.container');
+
+class PopupContainer {
+  constructor(stringMessage) {
+    this.stringMessage = stringMessage;
+    this.div = document.createElement('div');
+    this.paragraph = document.createElement('p');
+    this.buttonContainer = document.createElement('div');
+    this.doneButton = document.createElement('button');
+    this.deleteButton = document.createElement('button');
+    this.doneSpan = document.createElement('span');
+    this.deleteSpan = document.createElement('span');
+  }
+
+  appendToContainer() {
+    container.appendChild(this.div);
+    this.div.classList.add('popup');
+    this.div.appendChild(this.paragraph);
+    this.paragraph.textContent = this.stringMessage;
+    this.div.appendChild(this.buttonContainer);
+    this.buttonContainer.classList.add('button-container');
+    this.buttonContainer.appendChild(this.doneButton);
+    this.doneButton.classList.add('button');
+    this.doneButton.classList.add('done');
+    this.doneButton.appendChild(this.doneSpan);
+    this.doneSpan.classList.add('material-symbols-outlined');
+    this.doneSpan.textContent = ' done ';
+    this.buttonContainer.appendChild(this.deleteButton);
+    this.deleteButton.classList.add('button');
+    this.deleteButton.classList.add('delete');
+    this.deleteButton.appendChild(this.deleteSpan);
+    this.deleteSpan.classList.add('material-symbols-outlined');
+    this.deleteSpan.textContent = ' delete ';
+  }
+}
+
+const popupContainer = new PopupContainer();
+
+// EVENT LISTENERS
+submitBtn.addEventListener('click', () => {
+  if (inputField.value === '') {
+    return;
+  } else {
+    const popupContainer = new PopupContainer(inputField.value);
+    popupContainer.appendToContainer();
+    inputField.value = '';
+  }
+});
+
+// check
+container.addEventListener('click', (event) => {
+  if (event.target.classList[0] === 'input') {
+    return;
+  } else if (event.target.firstChild.innerHTML === ' done ') {
+    event.target.parentElement.parentElement.firstChild.innerHTML =
+      event.target.parentElement.parentElement.firstChild.innerHTML.strike();
+  } else if (event.target.innerHTML === ' done ') {
+    event.target.parentElement.parentElement.parentElement.firstChild.innerHTML =
+      event.target.parentElement.parentElement.parentElement.firstChild.innerHTML.strike();
+  } else return;
+});
+
+//  remove parent element with deleteBtn
+container.addEventListener('click', (event) => {
+  if (event.target.classList[0] === 'input') {
+    return;
+  } else if (event.target.firstChild.innerHTML === ' delete ') {
+    event.target.parentElement.parentElement.remove();
+  } else if (event.target.innerHTML === ' delete ') {
+    event.target.parentElement.parentElement.parentElement.remove();
+  } else return;
+});

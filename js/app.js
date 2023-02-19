32057c7acf878d1dbb45f83f08f9c88810dd4ead
Robert Lazić <robert.lazicpz@gmail.com>
2023-02-19 14:01:25 +0100

    modified:   js/app.js

--- js/app.js
+++ js/app.js
@@ -19,6 +19,7 @@ if (tasks.length !== 0) {
 submitBtn.addEventListener('click', () => {
   const inputValue = inputField.value;
   inputField.value = '';
+  inputField.focus();
   if (inputValue === '') {
     return;
   }
@@ -26,6 +27,7 @@ submitBtn.addEventListener('click', () => {
   const task = {
     id: new Date().getTime(),
     name: inputValue,
+    striked: false,
   };
 
   // add task in local storage
@@ -35,10 +37,39 @@ submitBtn.addEventListener('click', () => {
   createTask(task);
 });
 
+// submit with press Enter
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    const inputValue = inputField.value;
+    inputField.value = '';
+    inputField.focus();
+    if (inputValue === '') {
+      return;
+    }
+
+    const task = {
+      id: new Date().getTime(),
+      name: inputValue,
+      striked: false,
+    };
+
+    // add task in local storage
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+
+    createTask(task);
+  } else return;
+});
+
 // create todo task
 function createTask(tasksArray) {
   const id = tasksArray.id;
-  const name = tasksArray.name;
+  let name;
+  if (tasksArray.striked) {
+    name = tasksArray.name.strike();
+  } else if (tasksArray.striked === false) {
+    name = tasksArray.name;
+  }
   const todo = document.createElement('div');
   todo.setAttribute('id', id);
   todo.classList.add('popup');
@@ -87,16 +118,30 @@ todoContainer.addEventListener('click', (e) => {
 
 // strikethrough text of finished todo task
 todoContainer.addEventListener('click', (e) => {
+  let paraText;
+  let id;
   // if click on button
   if (e.target.className === 'button done') {
-    strikedParagraph =
-      e.target.parentNode.parentNode.firstElementChild.innerHTML =
-        e.target.parentNode.parentNode.firstElementChild.innerHTML.strike();
+    id = e.target.parentNode.parentNode.getAttribute('id');
+    paraText = e.target.parentNode.parentNode.firstElementChild.innerHTML;
+    e.target.parentNode.parentNode.firstElementChild.innerHTML =
+      paraText.strike();
   }
   // if click on span
   else if (e.target.parentNode.className === 'button done') {
-    strikedParagraph =
-      e.target.parentNode.parentNode.parentNode.firstElementChild.innerHTML =
-        e.target.parentNode.parentNode.parentNode.firstElementChild.innerHTML.strike();
+    id = e.target.parentNode.parentNode.parentNode.getAttribute('id');
+    paraText =
+      e.target.parentNode.parentNode.parentNode.firstElementChild.innerHTML;
+    e.target.parentNode.parentNode.parentNode.firstElementChild.innerHTML =
+      paraText.strike();
   }
+
+  // if task element is done set task.striked = true
+  tasks.forEach((task) => {
+    if (task.id == id) {
+      task.striked = true;
+    }
+  });
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 });
